368d03c5e2b2b80026d16e982caa490b7d237e11
József Sólya <solyaj@GDO.LAN>
2021-10-06 11:30:13 +0200

    tomeg

---
+++ infor.py
@@ -0,0 +1,3 @@
+varosok = ['Miskolc', 'Párizs','Dublin','Lajosmizse']
+for varos in varosok:
+    print(varos, 'egy varos eurobaban te buta földrajz bubi')  
---
+++ tomeg.py
@@ -0,0 +1,6 @@
+import random
+
+tomegek = []
+for _ in range(20):
+    tomegek.append(random.randint(1000,10000))
+print(tomegek)
