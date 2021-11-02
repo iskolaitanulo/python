50a917b785ce0f4a1f82bb079ef4c8f200536612
József Sólya <solyaj@GDO.LAN>
2021-11-02 11:34:05 +0100

    az

---
+++ lista/szorzotabla.py
@@ -0,0 +1,8 @@
+szorzandó = 1
+while szorzandó <= 15:
+    szorzó = 1
+    while szorzó <= 15:
+        print(szorzandó,'*',szorzó,'=',szorzandó*szorzó)
+        szorzó += 1
+    print('')
+    szorzandó += 1
