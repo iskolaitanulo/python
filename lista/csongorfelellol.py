0015a4b9679dab0a58f78a1901d752e31b385503
József Sólya <solyaj@GDO.LAN>
2021-11-30 11:25:46 +0100

    doga

--- lista/csongorfelellol.py
+++ lista/csongorfelellol.py
@@ -1,13 +1,32 @@
 import random
-szam1 = float(input("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"))
+
+nev = input("Kérem adja meg a nevét")
+szam1 = float(input("adjon meg egy számot"))
 szam2 = random.randrange(10 , 50)
 SZAM3 = 5
+#halmaz
+szamok2 = {23}
+szamok2.add(szam1)
+szamok2.add(szam2)
+szamok2.add(SZAM3)
+print(szamok2)
+#lista
 szamok = []
 szamok.append(szam1)
 szamok.append(szam2)
 szamok.append(SZAM3)
 if szam1 % 2 == 0:
-    print("elkap az agybaj")
+    print("páros")
 else:
-    print("nem kap el az agybaj")
+    print("Páratlan")
 print(szam1, szam2)
+szam4 = str(szam1)
+wr = open("jozsi.txt","w")
+wr.write(nev)
+wr.write(szam4)
+wr.close()
+
+lista = [1,2,3,4,5,"abc","def"]
+with open("jozsii.txt",'w') as file:
+    for item in lista:
+        file.write("%s\n" % item) 
