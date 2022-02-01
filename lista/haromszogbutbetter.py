5a8472a62e14eb33a1ffd7e9344b9bef618b0d6b
József Sólya <solyaj@GDO.LAN>
2022-02-01 11:35:54 +0100

    lol

---
+++ lista/haromszogbutbetter.py
@@ -0,0 +1,10 @@
+haromszog = [3,4,5]
+szerkeszhető = False
+
+if haromszog[0]+haromszog[1] > haromszog[2] and haromszog[0]+haromszog[2] > haromszog[1] and haromszog[1]+haromszog[2] > haromszog[0]:
+    print("Szerkezthető")
+    szerezthető = True
+else:
+
+    print("Nem szerezthető")
+#solya jozsef feladat megoldása :D
---
+++ lista/zsakos.py
@@ -0,0 +1,22 @@
+zsak = [2,9,19,20,5,7.11]
+wr.open('zsak.txt','w')
+nagyobb = 0
+for x in zsak:
+    if x >5
+        nagyobb += x
+        print(f'a 5kgnal nagyobb zsakok ossztomnege {nagyobb} kg') 
+        wr.write(\nf'a 5kgnal nagyobb zsakok ossztomnege {nagyobb} kg')
+
+napok = 0
+legnehezebb = 0
+legkonnyebb = 0
+for x in zsak
+    napok=zsak.index(x)+1
+    print(f'A {napok}. napon {x}kg-ot emelt meg')
+    if x >legnehezebb
+        legnehezebb = x
+    if x < legkonnyebb:
+        legkonyebb = x
+napok = zsak.index(legkonyebb)+1
+        print(f"A lekönyebb zsak {legkonnyebb}kg volt \n A legnehezebbb {legnehezebb} volt, ennyi napon{napok}")
+wr.close()
