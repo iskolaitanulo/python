40914fed7a80d5b42a61855b7ae784c0c4ead03d
József Sólya <solyaj@GDO.LAN>
2022-05-09 13:39:52 +0200

    heloszi

---
+++ worst on is on it's way/hordo.py
@@ -0,0 +1,13 @@
+l = 2400
+class Bor:
+    def __init__(self,bor):
+        self.bor = bor
+        self.nev = nev
+
+    def hordo(l):
+        return l/52,5
+vállalkozok = []
+for _ in range(3):
+    vallalkozoneve = input('Válalkozo neve')
+    bormennyiseg = int(input('mennyi bro bor'))
+    vállalkozok
